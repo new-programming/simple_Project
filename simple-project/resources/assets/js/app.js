4ad5cd41972885b6856aa1a728b2f507bf91ea82
test <test>
2021-01-10 23:26:13 +0900

    Vueのi18n導入

--- simple-project/resources/assets/js/app.js
+++ simple-project/resources/assets/js/app.js
@@ -10,11 +10,13 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import Vue from 'vue';
-
+import VueI18n from 'vue-i18n';
+import messages from './config/messages';
 import vue_routes from './components/vue_routes.js';
 import { store } from './store/store';
 
 Vue.use(vue_routes);
+Vue.use(VueI18n);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,7 +31,14 @@ for(var i in vue_routes) {
     Vue.component(tag, require('' + dir + file));
 }
 
+const i18n = new VueI18n({
+    locale: 'ja',
+    i18n: i18n,
+    messages: messages,
+})
+
 const app = new Vue({
     el: '#app',
-    store
+    store,
+    i18n: i18n,
 });
