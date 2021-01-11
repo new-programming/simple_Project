92e6723432283a1a75b077d8a2e16e1db4d0e546
test <test>
2021-01-11 21:37:26 +0900

    ホーム画面のヘッダー作成

--- simple-project/resources/assets/js/components/home/index.vue
+++ simple-project/resources/assets/js/components/home/index.vue
@@ -1,25 +1,67 @@
 <template>
-        <div class="container">
-        <header class="header">
-            <nav class="header__nav">
-                <div class="header__nav__item"></div>
-                <div class="header__nav__item"></div>
-                <div class="header__nav__item"></div>
-                <div class="header__nav__item"></div>
-                <div class="header__nav__item"></div>
-                <div class="header__nav__item"></div>
-            </nav>
-            <div class="header__entry">
-                <div class="header__entry__item"></div>
-                <div class="header__entry__item"></div>
-                <div class="header__entry__item"></div>
+
+        <!-- <div class="container"> -->
+            <!-- <header class="header">
+                <nav class="header__nav">
+                    <div class="header__nav__item"></div>
+                    <div class="header__nav__item"></div>
+                    <div class="header__nav__item"></div>
+                    <div class="header__nav__item"></div>
+                    <div class="header__nav__item"></div>
+                    <div class="header__nav__item"></div>
+                </nav>
+                <div class="header__entry">
+                    <div class="header__entry__item"></div>
+                    <div class="header__entry__item"></div>
+                    <div class="header__entry__item"></div>
+                </div>
+            </header> -->
+            <div>
+                <div class="header">
+                <nav class="header navbar navbar-expand-lg navbar-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Simple Project</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="https://www.google.co.jp">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    </div>
+                </div>
+                </nav>
             </div>
-        </header>
-        <main>
+        </div>
+
+        <!-- <main>
             <div class="hero">
                 <h1>{{$t("home.title")}}</h1>
-                <!-- <h1>MORE<br>EXCITING<br>EXPERIENCE</h1> -->
-            </div>
+                <! <h1>MORE<br>EXCITING<br>EXPERIENCE</h1> -->
+            <!-- </div>
             <section class="contents">
                 <h2 class="main-title">SERVICE</h2>
                 <div class="contents__item"></div>
@@ -33,11 +75,11 @@
                 <h2 class="main-title">CONCEPT</h2>
                 <p></p>
             </section>
-        </main>
-        <footer>
+        </main> -->
+        <!-- <footer>
             <p>&copy;simple-project</p>
-        </footer>
-    </div>    
+        </footer> -->
+    <!-- <div> -->
 </template>
 
 <script>
